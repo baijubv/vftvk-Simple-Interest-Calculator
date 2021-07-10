bf0badaa2d5c46ed327b94ca92eea61814edc26a
Baiju B V <bvb.ithits@gmail.com>
2021-07-10 17:50:56 +0000

    changes made by Baiju

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;  
-   var rate = document.getElementById("rate").value;
-   var years = document.getElementById("years").value;
+    var principal = parseFloat(document.getElementById("principal").value);  
+   var rate = parseFloat(document.getElementById("rate").value);
+   var years = parseInt (document.getElementById("years").value);
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     function updateRate() 
@@ -10,7 +10,20 @@ function compute()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+
 document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
-    
+    }
+
+    function getSliderValue() {
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
 }
         
+function validateAmount() {
+    var principal = document.getElementById("principal").value;
+    var biggerThanZero = parseInt(principal) > 0;
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+}
